a043a77395ee974df4597f6a30523d2880174107
DmitriiKumancev <dmitrijkumancev571@gmail.com>
2023-03-30 17:53:06 +0300

    update for deploy

--- store-backend/gamestore/settings.py
+++ store-backend/gamestore/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'DmitriyKuma.pythonanywhere.com']
 
 
 # Application definition
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
